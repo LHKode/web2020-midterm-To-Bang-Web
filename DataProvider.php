1294280402ac5bac11b04bb1848e60b242032822
pii3125 <duyenthimyphan31252000@gmail.com>
2020-07-04 10:54:52 +0700

    My Duyen upload Data Pro

---
+++ DataProvider.php
@@ -0,0 +1,25 @@
+<?php
+class DataProvider 
+{
+	public static function ExecuteQuery($sql)
+	{
+        $host="localhost";
+        $user="root";
+        $password="";
+		$database="qlsv_to_bang_web";
+		
+		try{
+			$connection = mysqli_connect($host,$user,$password,$database);//or 				die ("couldn't connect to localhost");				
+			mysqli_query($connection, "set names 'utf8'");		
+			$result = mysqli_query($connection, $sql);		
+            mysqli_close($connection);
+            // echo "Thành công";		
+			return $result;
+		}catch(Exception $ex)
+		{
+            echo "lỗi";
+			return null;
+		}
+	}
+}
+?>
